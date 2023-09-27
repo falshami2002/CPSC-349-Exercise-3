ff7a58fd509db27f9da0006a8b558d8b1b385b9e
falshami2002 <115384875+falshami2002@users.noreply.github.com>
2023-09-26 19:52:42 -0700

    Fixes

--- script.js
+++ script.js
@@ -16,64 +16,32 @@ btns.forEach(btn => btn.addEventListener("click", event => {
         equ.innerHTML = "";
     }
     else if (op.includes("=")) {
-        equ.innerHTML = parseFloat((recurse(equ.innerHTML)).toFixed(2));
+        equ.innerHTML = parseFloat((evaluate(equ.innerHTML)).toFixed(2));
     }
     else {
-        equ.innerHTML += op;
+        equ.innerHTML += op.trim();
     }
 }));
 
-/*
-//This function can only solve single operation equations.
 function evaluate(equation) {
     if (equation.includes("+")) {
-        let a = parseInt(equation.split('+')[0]);
-        let b = parseInt(equation.split('+')[1]);
+        let a = parseFloat(equation.split('+')[0]);
+        let b = parseFloat(equation.split('+')[1]);
         return a + b;
     }
     else if (equation.includes("-")) {
-        let a = parseInt(equation.split('-')[0]);
-        let b = parseInt(equation.split('-')[1]);
+        let a = parseFloat(equation.split('-')[0]);
+        let b = parseFloat(equation.split('-')[1]);
         return a - b;
     }
     else if (equation.includes("/")) {
-        let a = parseInt(equation.split('/')[0]);
-        let b = parseInt(equation.split('/')[1]);
+        let a = parseFloat(equation.split('/')[0]);
+        let b = parseFloat(equation.split('/')[1]);
         return a / b;
     }
     else if (equation.includes("x")) {
-        let a = parseInt(equation.split('x')[0]);
-        let b = parseInt(equation.split('x')[1]);
+        let a = parseFloat(equation.split('x')[0]);
+        let b = parseFloat(equation.split('x')[1]);
         return a * b;
-    }
-}*/
-
-function recurse(equation) {
-    let divIndex = equation.indexOf("/");
-    let multIndex = equation.indexOf("x");
-    let addIndex = equation.indexOf("+");
-    let subIndex = equation.indexOf("-");
-    if ((divIndex < multIndex && divIndex != -1) || (divIndex != multIndex && multIndex == -1)) {
-        let first = equation.substring(0, divIndex);
-        let second = equation.substring(divIndex + 1);
-        return recurse(first) / recurse(second);
-    }
-    else if ((divIndex > multIndex && multIndex != -1) || (divIndex != multIndex && divIndex == -1)) {
-        let first = equation.substring(0, multIndex);
-        let second = equation.substring(multIndex + 1);
-        return recurse(first) * recurse(second);
-    }
-    else if ((addIndex < subIndex && addIndex != -1) || (addIndex != subIndex && subIndex == -1)) {
-        let first = equation.substring(0, addIndex);
-        let second = equation.substring(addIndex + 1);
-        return recurse(first) + recurse(second);
-    }
-    else if ((addIndex > subIndex && subIndex != -1) || (addIndex != subIndex && addIndex == -1)) {
-        let first = equation.substring(0, subIndex);
-        let second = equation.substring(subIndex + 1);
-        return recurse(first) - recurse(second);
-    }
-    else {
-        return parseFloat(equation);
     }
 }
